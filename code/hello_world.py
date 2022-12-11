19c7afddc3702ca603fc72cac3066f91bb700af2
Sreekanth V K <sreekanth.vk@yahoo.com>
2022-12-11 12:05:23 +0530

    Update hello_world.py
    Preliminary code

--- code/hello_world.py
+++ code/hello_world.py
@@ -1,1 +1,15 @@
+!#/bin/python 
+# single line comment to print Hello World
+print("Hello World!")
+
+''' 
+An example for multiline comment or documentation
+Assign the value to the variable and use the variables
+Operate on variables
+'''
+hello = "Hello"
+world = "World"
+print(hello+world)
+
+
 
