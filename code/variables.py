52eeff050f5b0298d67f70fc6875a99659f57ace
Sreekanth V K <sreekanth.vk@yahoo.com>
2022-12-15 17:05:01 +0530

    Update variables.py

--- code/variables.py
+++ code/variables.py
@@ -1,1 +1,16 @@
+num1 = 1
+num2 = 2
+sum_of_nums = num1 + num2 
+print("hello" + str(num1))
 
+# variables
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0 
+
+# checking type of variables
+
+type(x)
+type(y)
+type(z)
+type(True)
